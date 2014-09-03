10d62e46236f207e7db65de262fe29aad0048f74
r1k <r.i.kelsey@gmail.com>
2014-09-03 11:30:54 +0100

    Reduce logging from websocketpp

--- eventServer/src/ClientWebSocketHandler.h
+++ eventServer/src/ClientWebSocketHandler.h
@@ -23,6 +23,9 @@ public:
         m_server.init_asio();
         m_server.set_open_handler(bind(&ClientWebSocketHandler::on_open, this, _1));
         m_server.set_close_handler(bind(&ClientWebSocketHandler::on_close, this, _1));
+
+        m_server.set_access_channels(websocketpp::log::alevel::none);
+        m_server.set_error_channels(websocketpp::log::elevel::rerror);
     }
 
     ~ClientWebSocketHandler()
